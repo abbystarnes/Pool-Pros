bfe05ee4b18cbd94924f18e415127551f4a2ac0d
Abby Starnes <abbymstarnes@gmail.com>
2017-10-20 15:56:19 -0400

    card style and filter functionality - in progress

--- public/js/main.js
+++ public/js/main.js
@@ -21,4 +21,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
     nav.classList.add("hidden");
     overlay.classList.add('hidden');
   });
+
+// sorting functionality
+  let cards = document.getElementsByClassName("card");
+  let cards_services = {};
+  for (let x = 0; x < cards.length; x++){
+    let card = cards[x];
+    let services = card.getElementsByClassName('card-service')[0].dataset.certifications.split(',');
+    cards_services[x] = services;
+  }
+
+  let checkboxes = document.getElementsByClassName('checkbox');
+
+  for(let x = 0; x < checkboxes.length; x++){
+    checkboxes[x].addEventListener("mousedown", function() {
+      let service_type = checkboxes[x].dataset.certifications;
+        for (let x = 0; x < cards.length; x++){
+          toggleCard(service_type, x);
+        }
+    });
+  }
+
+  let toggleCard = function(service, card_idx){
+    if (cards_services[card_idx].includes(`${service} Pro`)){
+      console.log('yep');
+      cards[card_idx].classList.toggle(`show-${service}`);
+    } else {
+      console.log('nope');
+    }
+  }
 });
